3c67f95d96a2c9bf31be5c9c6f89dc5456e7839a
bala-amypo <bala@amypo.in>
2025-12-17 09:58:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 public class StudentController{
 @Autowired StudentService ser//here actually StudentServiceImpl should be used ut we use StudentService as we dont want to show the logic to user for security purposes
 @PostMapping("/post")
-public sendData(){
-    return 
+public StudentEntity sendData(@RequestBody StudentEntity stu){
+    return ser.postData(stu);
 }
 }
