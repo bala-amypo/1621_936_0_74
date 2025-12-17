2dd5dc28664d8edda7482fad9defc8dbeeb4f792
bala-amypo <bala@amypo.in>
2025-12-17 09:53:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,5 +3,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 public class StudentController{
-@Autowired StudentService ser//here actually StudentServiceImpl 
+@Autowired StudentService ser//here actually StudentServiceImpl should be used ut we use StudentService as we dont want to show the logic to user for security purposes
+@PostMapping("/post")
+public sendData(){
+    return 
+}
 }
