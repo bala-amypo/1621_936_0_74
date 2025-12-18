0f2411877b0a0ac4ac0ef6d186f28e3ce7ada131
bala-amypo <bala@amypo.in>
2025-12-18 09:17:10 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -1,3 +1,25 @@
+// package com.example.demo.service.Impl;
+// import org.springframework.stereotype.Service;
+// import com.example.demo.service.ValidationService;
+// import  com.example.demo.entity.ValidationEntity;
+// import  com.example.demo.repository.ValidationRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import java.util.List;
+
+// @Service
+// public class ValidationServiceImpl implements ValidationService{
+//       @Autowired ValidationRepository student;
+//       //save()
+//       //findall()
+//       //findById()
+//       //deleteById()
+//       //existsById()
+//  @Override
+//  public ValidationEntity postdata(ValidationEntity stu){
+//     return student.save(stu);
+//  }
+// }
+
 package com.example.demo.service.Impl;
 import org.springframework.stereotype.Service;
 import com.example.demo.service.ValidationService;
@@ -9,13 +31,11 @@ import java.util.List;
 @Service
 public class ValidationServiceImpl implements ValidationService{
       @Autowired ValidationRepository student;
-      //save()
-      //findall()
-      //findById()
-      //deleteById()
-      //existsById()
- @Override
- public ValidationEntity postdata(ValidationEntity stu){
+     
+      @Override
+ public ValidationEntity postdataa(ValidationEntity stu){
     return student.save(stu);
  }
 }
+
+
