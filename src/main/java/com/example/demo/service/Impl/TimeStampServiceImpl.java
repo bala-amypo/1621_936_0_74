61558d399430f56d3826e2a7d9e88a932a2d615f
bala-amypo <bala@amypo.in>
2025-12-19 08:12:53 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class TimeStampServiceImpl implements TimeStampService{
-      @Autowired Repository student;
+      @Autowired TimeStampRepository student;
       //save()
       //findall()
       //findById()
